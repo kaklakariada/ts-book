c27cce579e20fb982997a2b65e2c562341174a14
kaklakariada <kaklakariada@users.noreply.github.com>
2016-03-02 20:13:39 +0100

    add tsc task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,22 @@
 
 var gulp = require('gulp');
 var tslint = require('gulp-tslint');
+var ts = require('gulp-typescript');
+
+var tsProject = ts.createProject({
+  removeComments: true,
+  noImplicitAny: true,
+  target: 'ES3',
+  module: 'commonjs',
+  declarationFiles: false
+});
+
+gulp.task('tsc', function() {
+  return gulp.src(['./source/ts/**/**.ts'])
+    .pipe(ts(tsProject))
+    .js.pipe(gulp.dest('./temp/source/js'));
+});
+
 
 gulp.task('lint', function() {
   return gulp.src(['./source/ts/**/**.ts', './test/**/**.test.ts'])
